03b5ccc758d23d57eff2e6ba6060a1e79ed398d5
blum <yuuybonito@gmail.com>
2023-09-16 06:27:52 -0300

    screen

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <GLFW/glfw3.h>
 #include <GL/gl.h>
-#include <GL/glext.h>
 
 /* typedefs */
 typedef char                b8;
@@ -12,6 +11,7 @@ typedef unsigned char       u8;
 typedef unsigned short      u16;
 typedef unsigned int        u32;
 typedef float               f32;
+typedef u32                 rgb;
 
 /* defines */
 #define MEM_AMOUNT 1024*8
@@ -57,6 +57,11 @@ typedef void    gl_buffer_data_fn(GLenum target, GLsizeiptr size, const void *da
 typedef void    gl_enable_vertex_attrib_array_fn(GLuint index);
 typedef void    gl_vertex_attrib_pointer_fn(GLuint index, GLint size, GLenum type, GLboolean normalized, GLsizei stride, const void *pointer);
 typedef void    gl_draw_elements_fn(GLenum mode, GLsizei count, GLenum type, const void *indices);
+typedef void    gl_gen_textures_fn(GLsizei n, GLuint *textures);
+typedef void    gl_bind_texture_fn(GLenum target, GLuint texture);
+typedef void    gl_tex_parameteri_fn(GLenum target, GLenum pname, GLint param);
+typedef void    gl_tex_image_2d(GLenum target, GLint level, GLint internal_format, GLsizei w, GLsizei h, GLint border, GLenum format, GLenum type, const void *data);
+typedef void    gl_tex_sub_image_2d(GLenum target, GLint level, GLint xoffset, GLint yoffset, GLsizei width, GLsizei height, GLenum format, GLenum type, const void * pixels);
 
 /* opengl functions */
 static gl_clear_fn                       *_glClear;
@@ -80,26 +85,43 @@ static gl_buffer_data_fn                 *glBufferData;
 static gl_enable_vertex_attrib_array_fn  *glEnableVertexAttribArray;
 static gl_vertex_attrib_pointer_fn       *glVertexAttribPointer;
 static gl_draw_elements_fn               *_glDrawElements;
-#define glClear       _glClear
-#define glClearColor  _glClearColor 
-#define glDrawElements _glDrawElements
+static gl_gen_textures_fn                *_glGenTextures;
+static gl_bind_texture_fn                *_glBindTexture;
+static gl_tex_parameteri_fn              *_glTexParameteri;
+static gl_tex_image_2d                   *_glTexImage2D;
+static gl_tex_sub_image_2d               *_glTexSubImage2D;
+#define glClear          _glClear
+#define glClearColor     _glClearColor 
+#define glDrawElements   _glDrawElements
+#define glGenTextures    _glGenTextures
+#define glBindTexture    _glBindTexture
+#define glTexParameteri  _glTexParameteri
+#define glTexImage2D     _glTexImage2D
+#define glTexSubImage2D  _glTexSubImage2D
 
 /* global variables */
 static u8 game_memory[MEM_AMOUNT]; /* basically all the memory we will ever need */
+static rgb screen[GAME_W*GAME_H];
+static rgb palette[4] = { 0x9bbc0f, 0x8bac0f, 0x306230, 0x0f380f };
 static s8 *vert_src =
 "#version 330 core\n"
 "layout (location = 0) in vec2 a_pos;\n"
+"layout (location = 1) in vec2 a_uv;\n"
+"out vec2 v_uv;"
 "void\n"
 "main() {\n"
 "  gl_Position = vec4(a_pos, 0.0, 1.0);\n"
+"  v_uv = a_uv;"
 "}\n"
 "\n";
 static s8 *frag_src =
 "#version 330 core\n"
 "out vec4 f_col;\n"
+"in vec2 v_uv;"
+"uniform sampler2D tex;"
 "void\n"
 "main() {\n"
-"  f_col = vec4(1.0, 1.0, 0.0, 1.0);\n"
+"  f_col = texture(tex, v_uv);\n"
 "}\n"
 "\n";
 
@@ -128,6 +150,7 @@ main(void) {
   const GLFWvidmode *vidmode;
   u32 shader;
   u32 vao, vbo, ibo;
+  u32 screen_tex;
 
   (void)game_memory;
 
@@ -178,6 +201,11 @@ main(void) {
   glEnableVertexAttribArray  = (gl_enable_vertex_attrib_array_fn *)glfwGetProcAddress("glEnableVertexAttribArray");
   glVertexAttribPointer      = (gl_vertex_attrib_pointer_fn *)glfwGetProcAddress("glVertexAttribPointer");
   glDrawElements             = (gl_draw_elements_fn *)glfwGetProcAddress("glDrawElements");
+  glGenTextures              = (gl_gen_textures_fn *)glfwGetProcAddress("glGenTextures");
+  glBindTexture              = (gl_bind_texture_fn *)glfwGetProcAddress("glBindTexture");
+  glTexParameteri            = (gl_tex_parameteri_fn *)glfwGetProcAddress("glTexParameteri");
+  glTexImage2D               = (gl_tex_image_2d *)glfwGetProcAddress("glTexImage2D");
+  glTexSubImage2D            = (gl_tex_sub_image_2d *)glfwGetProcAddress("glTexSubImage2D");
 
   /* make shader */
   {
@@ -205,10 +233,11 @@ main(void) {
   /* buffers */
   {
     f32 vertices[] = {
-      -0.5f, -0.5f,
-       0.5f, -0.5f,
-       0.5f,  0.5f,
-      -0.5f,  0.5f,
+    /* pos              uv    */
+      -1.0f, -1.0f,     0, 1,
+       1.0f, -1.0f,     1, 1,
+       1.0f,  1.0f,     1, 0,
+      -1.0f,  1.0f,     0, 0
     };
     u32 indices[] = {
       0, 1, 2,
@@ -223,17 +252,42 @@ main(void) {
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof (indices), indices, GL_STATIC_DRAW);
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof (f32) * 2, (void *)0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof (f32) * 4, (void *)0);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof (f32) * 4, (void *)(sizeof (f32) * 2));
   }
 
-  while (!glfwWindowShouldClose(window)) {
-    glClearColor(0.8f, 0.2, 0.2f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-    glfwPollEvents();
-    glfwSwapBuffers(window);
-  }
+  /* screen */
+  glGenTextures(1, &screen_tex);
+  glBindTexture(GL_TEXTURE_2D, screen_tex);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, GAME_W, GAME_H, 0, GL_RGBA, GL_UNSIGNED_BYTE, screen);
 
+  {
+    u32 offset = 0;
+    f32 increament = 0;
+    while (!glfwWindowShouldClose(window)) {
+      u32 x, y;
+      glClearColor(0.8f, 0.2, 0.2f, 1.0f);
+      glClear(GL_COLOR_BUFFER_BIT);
+      increament += 0.5f;
+      if (increament > 1.0f) {
+        offset++;
+        increament = 0;
+      }
+      for (y = 0; y < GAME_H; y++) {
+        for (x = 0; x < GAME_W; x++) {
+          u32 i = y * GAME_W + x;
+          screen[i] = palette[(x + y + offset) % 4];
+        }
+      }
+      glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, GAME_W, GAME_H, GL_RGBA, GL_UNSIGNED_BYTE, screen);
+      glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+      glfwPollEvents();
+      glfwSwapBuffers(window);
+    }
+  }
   /* exit glfw */
   glfwTerminate();
   return 0;
